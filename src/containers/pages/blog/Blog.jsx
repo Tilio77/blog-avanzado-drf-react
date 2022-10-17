f1523105ed1d55aab56451ae93b8b97ed6978521
Tilio77 <tiliobolso1@gmail.com>
2022-10-17 13:19:47 -0300

    Paginacion arreglada

--- src/containers/pages/blog/Blog.jsx
+++ src/containers/pages/blog/Blog.jsx
@@ -9,10 +9,12 @@ import { get_blog_list, get_blog_list_page } from "redux/actions/blog";
 function Blog({
     get_blog_list,
     get_blog_list_page,
-    blog_list
+    blog_list,
+    count
 }){
 
     useEffect(()=>{
+        window.scrollTo(0,0)
         get_blog_list()
     },[])
 
@@ -20,13 +22,14 @@ function Blog({
         <FullWidthLayout>
             <Header/>
             <BlogCategories/>
-            <BlogList get_blog_list_page={get_blog_list_page} blog_list={blog_list}/>
+            <BlogList get_blog_list_page={get_blog_list_page} blog_list={blog_list} count={count && count} />
         </FullWidthLayout>
     )
 }
 
 const mapStateToProps = state =>({
     blog_list: state.blog.blog_list,
+    count: state.blog.count
 })
 
 export default connect(mapStateToProps,{
