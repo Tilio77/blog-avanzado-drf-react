993ba8213ee61ee6de8f766fee5d52aa12d4377a
Tilio77 <tiliobolso1@gmail.com>
2022-10-17 12:41:38 -0300

    Cambios web

--- src/hocs/layouts/FullWidthLayout.js
+++ src/hocs/layouts/FullWidthLayout.js
@@ -1,21 +1,34 @@
-import { connect } from 'react-redux';
-import Navbar from 'components/navigation/Navbar';
-import Footer from 'components/navigation/Footer';
+import Footer from "components/navigation/Footer"
+import Navbar from "components/navigation/Navbar"
+import { useEffect } from "react"
+import { connect } from "react-redux"
+import { get_categories } from "redux/actions/categories"
 
-const FullWidthLayout = ({ children }) => {
-	return(
-		<>
-		<Navbar />
-		{children}
-		<Footer />
-		</>
-	);
+
+const FullWidthLayout = ({children, categories}) => {
+
+    useEffect(()=>{
+        categories ? <></>:get_categories()
+    },[])
+    return(
+        <>
+        <Navbar/>
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        {/* We've used 3xl here, but feel free to try other max-widths based on your needs */}
+            <div className="max-w-7xl mx-auto">
+                {/* Content goes here */}
+                {children}
+            </div>
+        </div>
+        <Footer/>
+        </>
+    )
 }
 
-const mapStateToProps = state => ({
-
+const mapStateToProps = state =>({
+    categories: state.categories.categories
 })
 
-export default connect(mapStateToProps, {
+export default connect(mapStateToProps,{
 
 })(FullWidthLayout)
